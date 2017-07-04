af94d68fd58d978119300019d92186558f6daa9a
ADESSO\matthias.kapferer <matthias.kapferer@gepardec.com>
2017-07-04 12:51:29 +0200

    First attempt to run on Openshift

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,24 @@
 FROM debian:latest
-MAINTAINER Roberto Andrade <roberto@cloud.com>
+
+MAINTAINER M0nt3c1t0 <montecito.games@gmail.com>
+
+ENV RUN_USER 777
+ENV RUN_GROUP 0
 ENV DEBIAN_FRONTEND noninteractive
 ENV TERM xterm
 
 RUN apt-get update && \
-	apt-get install -y wget dnsutils vim telnet && \
-	echo 'deb http://download.jitsi.org/nightly/deb unstable/' >> /etc/apt/sources.list && \
-	wget -qO - https://download.jitsi.org/nightly/deb/unstable/archive.key | apt-key add - && \
+	apt-get install -y sudo libnss-wrapper wget dnsutils vim telnet nano curl apt-transport-https gnupg && \
+	sh -c "echo 'deb https://download.jitsi.org stable/' > /etc/apt/sources.list.d/jitsi-stable.list" && \
+	wget -qO - https://download.jitsi.org/jitsi-key.gpg.key | sudo apt-key add - && \
 	apt-get update && \
 	apt-get -y install jitsi-meet && \
 	apt-get clean
 
+RUN useradd --uid 777 -m -g jitsi -G root -s /bin/bash jitsi-meet && \
+	echo "%root ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+#RUN adduser jitsi root
 #ENV PUBLIC_HOSTNAME=192.168.59.103
 
 #/etc/jitsi/meet/localhost-config.js = bosh: '//localhost/http-bind',
@@ -21,4 +29,6 @@ EXPOSE 80 443 5347
 EXPOSE 10000/udp 10001/udp 10002/udp 10003/udp 10004/udp 10005/udp 10006/udp 10007/udp 10008/udp 10009/udp 10010/udp
 
 COPY run.sh /run.sh
+
+USER ${RUN_USER}:${RUN_GROUP}
 CMD /run.sh
--- run.sh
+++ run.sh
@@ -1,25 +1,37 @@
+#!/bin/bash
 unset DEBIAN_FRONTEND
 
-export LOG=/var/log/jitsi/jvb.log
+export LOGDIR=/var/log/jitsi
+export LOG=$LOGDIR/jvb.log
+
+cat /etc/passwd > /tmp/passwd
+echo "$(id -u):x:$(id -u):$(id -g):dynamic uid:/tmp:/bin/false" >> /tmp/passwd
+
+cat /etc/group > /tmp/group
+echo "$(id -u):x:$(id -u):" >> /tmp/group
+
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/tmp/group
+export LD_PRELOAD=libnss_wrapper.so
 
 if [ ! -f "$LOG" ]; then
 	
 	sed 's/#\ create\(.*\)/echo\ create\1 $JICOFO_AUTH_USER $JICOFO_AUTH_DOMAIN $JICOFO_AUTH_PASSWORD/' -i /var/lib/dpkg/info/jitsi-meet-prosody.postinst
 
-	dpkg-reconfigure jitsi-videobridge
-	rm /etc/jitsi/jicofo/config && dpkg-reconfigure jicofo
-	/var/lib/dpkg/info/jitsi-meet-prosody.postinst configure
-	dpkg-reconfigure jitsi-meet
+	sudo dpkg-reconfigure jitsi-videobridge
+	sudo rm /etc/jitsi/jicofo/config && sudo dpkg-reconfigure jicofo
+	sudo /var/lib/dpkg/info/jitsi-meet-prosody.postinst configure
+	sudo dpkg-reconfigure jitsi-meet
 
-	touch $LOG && \
-	chown jvb:jitsi $LOG
+	sudo touch $LOG && \
+	sudo chown jvb:jitsi $LOG && sudo chmod -R 775 $LOGDIR
 fi
 
 cd /etc/init.d/
 
-./prosody restart && \
-./jitsi-videobridge restart && \
-./jicofo restart && \
-./nginx restart
+sudo ./prosody restart && \
+sudo ./jitsi-videobridge restart && \
+sudo ./jicofo restart && \
+sudo ./nginx restart
 
 tail -f $LOG
